8b14030116b6112bdd10bd5b9be19c6f7f6881ea
Me <johnpuliatch@gmail.com>
2021-07-06 15:03:46 -1000

    complete objective8lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,10 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+    for (int num=1; num < 11; num++){
+      System.out.println(num);
+    }
+    for (int num=10; num > 0; num--){
+      System.out.println(num);
+    }
+  }
+}
